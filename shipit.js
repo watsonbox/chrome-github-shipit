5352107eebc1114f9ac081288e721c8fa9f67821
Howard Wilson <howard@watsonbox.net>
2018-09-18 11:24:10 +0200

    Fix for latest GH changes

--- shipit.js
+++ shipit.js
@@ -3,7 +3,8 @@ base = window.location.href.split('...')[0].replace('compare/last_release', '');
 
 $('.gh-header-title').text('Getting latest release...')
 
+
 $.get(base + '/releases').done(function(data) {
-  url = base + 'compare/' + $('.tag-name', data)[0].innerHTML + "..." + branch;
+  url = base + 'compare/' + $('.commit-title', data)[0].innerText.replace(/[^0-9.v]+/g, "") + "..." + branch;
   $("#js-repo-pjax-container").load(url + " #js-repo-pjax-container");
 });
